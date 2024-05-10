4b59f2528b68dbeb4a2dd907ea412b1e2167a5e4
Daniil <gggchaseggg@gmail.com>
2024-05-10 15:16:15 +0300

    add metrics placeholder

--- src/pages/Metrics/Metrics.tsx
+++ src/pages/Metrics/Metrics.tsx
@@ -1,8 +1,11 @@
 export const Metrics = () => {
   return (
-    <iframe
-      style={{ width: "100%", height: "85vh" }}
-      src="https://datalens.yandex/n3i3ffzdbh3eb?_embedded=1&_no_controls=1"
-    ></iframe>
+    <>
+      <iframe
+        style={{ width: "100%", height: "85vh" }}
+        src="https://datalens.yandex/n3i3ffzdbh3eb?_embedded=1&_no_controls=1"
+      ></iframe>
+      {/* Место для новых метрик... */}
+    </>
   );
 };
